272a388b65e41de25c115c2ade96099010f71d7b
rensabella <108339569+rensabella@users.noreply.github.com>
2022-11-22 16:48:29 +0800

    bin

---
+++ bin/Supfig/blast_promoter_coverage.sh
@@ -0,0 +1,11 @@
+for s1 in AT NB SL OS ZM
+do
+for s2 in AT NB SL OS ZM
+do
+if [ "$s1" != "$s2" ]
+then
+bedtools multicov -bed <(awk '$4>0' promoter_site_${s1}_${s2}_blast|awk '$8>0'|cut -f3-5) -bams rmdup/${s1}_GLK1.bam rmdup/${s1}_GLK2.bam > promoter_coverage_${s1}_${s2}_${s1}
+bedtools multicov -bed <(awk '$4>0' promoter_site_${s1}_${s2}_blast|awk '$8>0'|cut -f7-9) -bams rmdup/${s2}_GLK1.bam rmdup/${s2}_GLK2.bam > promoter_coverage_${s1}_${s2}_${s2}
+fi
+done
+done
